af1f1db70999c88a8bfc69a3e9a4c0c256f6ed5e
nestor41 <nestor_infante@hotmail.com>
2013-11-06 23:04:27 -0600

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+	internal sealed class Program
+	{
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
